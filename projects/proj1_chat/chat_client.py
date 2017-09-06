3fad66a1a6295d86dd2c999e6f871725e00b29b4
Luis Torres <ltorres@berkeley.edu>
2017-09-05 21:21:09 -0700

    moved these files to private repo

---
+++ projects/proj1_chat/chat_client.py
@@ -0,0 +1,5 @@
+###Chat Client - project 1 CS 168
+### by Luis Torres
+###Inspired by "http://www.binarytides.com/code-chat-application-server-client-sockets-python/"
+import socket, select, sys
+from utils import *
--- projects/proj1_chat/chat_server.py
+++ projects/proj1_chat/chat_server.py
@@ -1,7 +1,10 @@
-#chat server - project 1 CS 168
-#  by Luis Torres
+###Chat Server - project 1 CS 168
+###  by Luis Torres
+###Inspired by "http://www.binarytides.com/code-chat-application-server-client-sockets-python/"
 import socket
+import select
 import sys
+from utils import *
 
 args = sys.argv
 
@@ -17,17 +20,35 @@ class ChatServer(object):
         self.socket = socket.socket()
         self.socket.bind(("", int(port)))
         self.socket.listen(5)
+        self.connections = []
+
+    def broadcast(self, sock, message):
+        for socket in self.connections:
+            if socket != self.socket and socket != sock:
+                try:
+                    socket.send(message)
+                except:
+                    socket.close()
+                    self.connections.remove(socket)
 
     def start(self):
         while True:
             """will need to implement more features"""
-            (new_socket, address) = self.socket.accept()
-            msg = new_socket.recv(1024)
-            tmp = msg
-            while tmp:
-                tmp = new_socket.recv(1024)
-                msg += tmp
-            print msg
+            read, write, error = select.select(self.connections, [], [])
+            for socket in read:
+                ###New Connection
+                if socket == self.socket:
+                    (new_socket, address) = self.socket.accept()
+                    self.connections.append(new_socket)
+                    self.broadcast(socket, "Welcome to the ChatServer!")
+                else:
+                    try:
+                        message = socket.recv(4096)
+                        if data:
+                            self.broadcast(socket,"[" + str(socket.getpeername()) + "]" + data)
+                    except:
+                        socket.close()
+                        self.connections.remove(socket)
 
 if __name__ == '__main__':
     if len(args) != 2:
